862824be3b34077bfb2e877a42c4b68e77939694
Coeus <bashee.wang@gmail.com>
2026-06-11 12:35:34 +0800

    Enable local search module: easyops-cn/docusaurus-search-local

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,6 +89,35 @@ const config = {
         },
     ],
 
+    themes: [
+        // ... Your other themes.
+        [
+            require.resolve("@easyops-cn/docusaurus-search-local"),
+            ({
+                // ... Your options.
+                // `hashed` is recommended as long-term-cache of index file is possible.
+                hashed: true,
+
+                // For Docs using Chinese, it is recomended to set:
+                language: ["en", "zh"],
+
+                // Customize the keyboard shortcut to focus search bar (default is "mod+k"):
+                searchBarShortcutKeymap: "ctrl+/", // Use 'S' key
+                // searchBarShortcutKeymap: "ctrl+shift+f", // Use Ctrl+Shift+F
+
+                // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+                forceIgnoreNoIndex: true,
+
+                // Enable Ask AI integration:
+                // askAi: {
+                //   project: "your-project-name",
+                //   apiUrl: "https://your-api-url.com/api/stream",
+                //   hotkey: "cmd+I", // Optional: keyboard shortcut to trigger Ask AI
+                // },
+            }),
+        ],
+    ],
+
     themeConfig:
         /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
